26ff733551878a9a1460d54e218d9f0bbf38193c
Bryce <brhowarth@outlook.com>
2020-01-13 11:15:54 -0700

    First Unity Lesson
    Updated Unity UI and practiced console printing with NewClass script

---
+++ Project1/Assets/Scripts/NewClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    private void Start()
+    {
+        print("Hello World");
+    }
+
+    private void Update()
+    {
+        print("Hello World, I'm never going to stop");
+    }
+}
