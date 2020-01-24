b4ca53caafa223e3c87ac5daa30e4e164b230a2c
Bryce <brhowarth@outlook.com>
2020-01-24 14:59:14 -0700

    Particle and Bullet Script Begun

---
+++ Project1/Assets/Scripts/Bullets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Instantiate("Bullet");
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            gameObject.SetActive(false);
+        }
+        if (other.gameObject.tag == "Ground")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
