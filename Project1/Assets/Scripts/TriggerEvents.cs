21bbdc414d0be2be277898fa6fc6ef6290f31bd0
Bryce <brhowarth@outlook.com>
2020-01-28 14:07:13 -0700

    OnTriggerExit OnTriggerStay Sprites

--- Project1/Assets/Scripts/TriggerEvents.cs
+++ Project1/Assets/Scripts/TriggerEvents.cs
@@ -1,11 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor.SceneManagement;
+using UnityEngine;
 using UnityEngine.Events;
 public class TriggerEvents : MonoBehaviour
 {
     public UnityEvent triggerEnterEvent;
-    
+
+    public bool stay;
+    public bool exit;
     private void OnTriggerEnter(Collider other)
     {
         triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (stay)
+        {
+            print("I'm here!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (exit)
+        {
+            print("I'm leaving!");
+            gameObject.SetActive(false);
+        }
     }
 }
