b096b56ea7bbb6d545dbcd047a0ed2a7bd48fbaf
Bryce <brhowarth@outlook.com>
2020-02-03 10:35:40 -0700

    Damage Health from Bullets
    https://www.youtube.com/watch?v=tzEVJ3tKQUg&t=659s followed health system guide to create UI and damage from bullets

---
+++ Project1/Assets/Scripts/DamageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    [SerializeField] private float bulletDamage;
+    [SerializeField] private HealthController healthController;
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Damage();
+        }
+    }
+
+    void Damage()
+    {
+        healthController.playerHealth = healthController.playerHealth - bulletDamage;
+        healthController.UpdateHealth();
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Project1/Assets/Scripts/HealthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public float playerHealth;
+    [SerializeField] private Text currentHealthText;
+
+
+    private void Start()
+    {
+        UpdateHealth();;
+    }
+
+    public void UpdateHealth()
+    {
+        //converts currentHealth float value into a string value to allow it to be put into text box
+        currentHealthText.text = playerHealth.ToString("0");
+    }
+}
