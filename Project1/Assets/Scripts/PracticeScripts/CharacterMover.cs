d691e634ae8ee06cacbd569dd5e78dea5b4c18cc
Bryce <brhowarth@outlook.com>
2020-02-05 11:17:00 -0700

    Character Move

---
+++ Project1/Assets/Scripts/PracticeScripts/CharacterMover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 positionDirection;
+    public float speed = 10f;
+    public float gravity = 3f;
+    public float jumpForce = 10f;
+
+    public bool isGrounded;
+    
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            positionDirection.y = jumpForce;
+        }
+        
+        positionDirection.y -= gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
