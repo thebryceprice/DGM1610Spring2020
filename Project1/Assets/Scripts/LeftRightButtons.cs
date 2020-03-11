99aac3ce254df9832ed4b0e3185780d58971db7b
Bryce <brhowarth@outlook.com>
2020-03-10 23:06:39 -0600

    Buttons

---
+++ Project1/Assets/Scripts/LeftRightButtons.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LeftRightButtons : MonoBehaviour
+{
+    public UnityEvent leftButton;
+    public UnityEvent rightButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftButton.Invoke();
+        rightButton.Invoke();
+    }
+
+    public void OnMouseDown()
+    {
+        transform.right = Vector3.left;
+        transform.right = Vector3.right;
+    }
+}
