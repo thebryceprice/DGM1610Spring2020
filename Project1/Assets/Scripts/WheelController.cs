13602d3d61863ed7f03478fc8e42c53261ba8224
Bryce <brhowarth@outlook.com>
2020-03-11 21:50:17 -0600

    Added Force and Drag
    Followed a guide by "Ottiya" that taught how to use the exponential function y=a^x to gradually slow down  a force applied to an object

---
+++ Project1/Assets/Scripts/WheelController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+//initial "speed" is set at 0
+    public float rotationSpeed = 0;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        //on every update, while mouse is down, apply rotation value of 10 to objects zed axis
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotationSpeed = 15;
+        }
+        transform.Rotate(0,this.rotationSpeed,0);
+        this.rotationSpeed *= 0.99f;
+    }
+}
