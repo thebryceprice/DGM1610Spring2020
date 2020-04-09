5dcc353c2e445fd1a1f4870e5c349fc0a3435fe8
Bryce <brhowarth@outlook.com>
2020-04-08 21:03:21 -0600

    Scene Load on Level Completion
    Added a script and attached to a capsule. On player collision the next scene will be loaded

---
+++ Project1/Assets/Scripts/LevelCompletion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCompletion : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
