3073fb1e37303955f0940f82131a90829a1778c4
Bryce <brhowarth@outlook.com>
2020-04-22 23:52:36 -0600

    Disappearing Platforms
    Script to disable certain platforms on player collision

---
+++ Project1/Assets/Scripts/VanishingPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VanishingPlatform : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Disappear();
+        }
+    }
+
+    
+    void Disappear()
+    {
+        gameObject.SetActive(false);
+    }
+}
